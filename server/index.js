ce89c7c986a9c57bd0ea73543713c822f6d2183f
NeelGosavi <neelgosavi07@gmail.com>
2025-12-19 14:42:39 +0530

    added

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ dotenv.config();
 const app = express();
 
 // 🔴 these must be BEFORE routes
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
