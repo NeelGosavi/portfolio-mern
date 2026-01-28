13ddf99157277cd0962242de0104c13e3e255c79
NeelGosavi <neelgosavi07@gmail.com>
2026-01-28 17:47:24 +0530

    Minor

--- client/src/api.js
+++ client/src/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // and the Render URL when deployed.
 const API_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5000/api"
-  : "https://portfolio-backend-otxk.onrender.com/";
+  : "https://portfolio-backend-otxk.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_URL,
